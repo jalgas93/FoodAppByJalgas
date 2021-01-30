6021d05bfaf2fe0ae008d1f901b37ce839e39d04
jalgas93 <jalgas1993@list.ru>
2021-01-30 10:12:29 +0500

    three commit

--- app/src/main/java/com/example/foodappbyjalgas/presentation/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/foodappbyjalgas/presentation/ui/recipe/RecipeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.foodappbyjalgas.R
 import com.example.foodappbyjalgas.util.App
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,12 +18,23 @@ import javax.inject.Inject
 class RecipeFragment : Fragment() {
 
 
-
     private val viewModel: RecipeViewModel by viewModels()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recipe, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getRecipe()
+    }
+
+    private fun getRecipe() {
+        viewModel.recipeLiveData.observe(viewLifecycleOwner, Observer {
+
+        })
     }
 
 
